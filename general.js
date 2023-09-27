5d2fcd770227f0c3b84e6ff4f1930ba5143e54a3
DBoudero <damien.boudero@laposte.net>
2023-09-27 15:07:30 +0200

    Jeu target

--- general.js
+++ general.js
@@ -1,10 +1,4 @@
-const isaac = document.querySelector('#isaac')
-
-let position = { x: 0, y: 0 };
-let IS_MOVING_RIGHT = false
-let IS_MOVING_LEFT = false
-let IS_MOVING_UP = false
-let IS_MOVING_DOWN = false
+// Sound effect
 
 function playSoundClick1() {
     var audio = new Audio('sounds/Click1.mp3');
@@ -16,25 +10,41 @@ function playSoundClick2() {
     audio.play();
 }
 
+// Mouvement
+
+const player = document.querySelector('#player');
+const target = document.querySelector('#target');
+const body = document.querySelector('body');
+let fontSize = player.style.fontSize
+let position = { x: generateRandomNumber(0, 100 - player.offsetWidth / document.documentElement.clientWidth * 100), y: generateRandomNumber(0, 100 - player.offsetHeight / document.documentElement.clientHeight * 100) };
+let IS_MOVING_RIGHT = false
+let IS_MOVING_LEFT = false
+let IS_MOVING_UP = false
+let IS_MOVING_DOWN = false
+
 window.requestAnimationFrame(deplacement)
 
 function deplacement() {
-    if (IS_MOVING_RIGHT & position.x < 100) {
+    if (IS_MOVING_RIGHT & position.x < 100 - player.offsetWidth / document.documentElement.clientWidth * 100) {
         position.x++;
     }
-    if (IS_MOVING_LEFT & position.x > 0) {
+    if (IS_MOVING_LEFT & position.x > 0) { // ne doit pas passer en dessous de 0
         position.x--;
     }
-    if (IS_MOVING_UP & position.y > 0) {
-        position.y--;
-    }
-    if (IS_MOVING_DOWN & position.y < 100) {
+    if (IS_MOVING_DOWN & position.y < 100 - player.offsetHeight / document.documentElement.clientHeight * 100) {
         position.y++;
     }
-    console.table(position);
+    if (IS_MOVING_UP & position.y > 0) { // ne doit pas passer en dessous de 0
+        position.y--;
+    }
 
-    isaac.style.left=`${position.x}%`
-    isaac.style.top=`${position.y}%`
+    if (elementsOverlap(player, target)){
+        targetMove()
+    }
+
+
+    player.style.left = `${position.x}%`
+    player.style.top = `${position.y}%`
 
     window.requestAnimationFrame(deplacement)
 }
@@ -58,4 +68,42 @@ function mouvement(event) {
             IS_MOVING_RIGHT = event.type === "keydown";
             break;
     }
+}
+
+// Souris Joueur
+
+function playerSouris1() {
+    player.innerHTML = " o/\n/| \n/ \\"
+}
+function playerSouris2() {
+    player.innerHTML = " o \n/|\\\n/ \\"
+}
+
+// Check target et joueur
+target.style.left = `${generateRandomNumber(0, 100 - target.offsetWidth / document.documentElement.clientWidth * 100)}%`
+target.style.top = `${generateRandomNumber(0, 100 - target.offsetHeight / document.documentElement.clientHeight * 100)}%`
+var score = 0
+
+
+function elementsOverlap(div1, div2) {
+    const domRect1 = div1.getBoundingClientRect();
+    const domRect2 = div2.getBoundingClientRect();
+
+    return !(
+        domRect1.top > domRect2.bottom ||
+        domRect1.right < domRect2.left ||
+        domRect1.bottom < domRect2.top ||
+        domRect1.left > domRect2.right
+    );
+}
+
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function targetMove() {
+    target.style.left = `${generateRandomNumber(0, 100 - target.offsetWidth / document.documentElement.clientWidth * 100)}%`
+    target.style.top = `${generateRandomNumber(0, 100 - target.offsetHeight / document.documentElement.clientHeight * 100)}%`
+    score++
+    document.title = "Score : " + score
 }
